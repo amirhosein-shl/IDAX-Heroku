57ef334ec35320b2d46e7d0a668a26138df239ed
Amir Shahlaee <68473050+amirhosein-shl@users.noreply.github.com>
2024-01-23 14:24:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -356,31 +356,31 @@ def upload_excel():
         app.config['EXCEL_FILE'] = excel_save_path 
     return redirect(url_for('index'))
         
-def index():
-    form = UploadFileForm()
-    if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        filename = secure_filename(file.filename)
-        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(save_path)
-        # global input1
-        # input1 = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))
-        # file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+# def index():
+#     form = UploadFileForm()
+#     if form.validate_on_submit():
+#         file = form.file.data # First grab the file
+#         filename = secure_filename(file.filename)
+#         save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#         file.save(save_path)
+#         # global input1
+#         # input1 = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))
+#         # file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
 
-        with zipfile.ZipFile(save_path, 'r') as zip_ref:
-            extract_path = os.path.join(app.config['UPLOAD_FOLDER'], 'unzipped')
-            zip_ref.extractall(extract_path)
+#         with zipfile.ZipFile(save_path, 'r') as zip_ref:
+#             extract_path = os.path.join(app.config['UPLOAD_FOLDER'], 'unzipped')
+#             zip_ref.extractall(extract_path)
 
-        app.config['EXTRACTED_FOLDER'] = extract_path
+#         app.config['EXTRACTED_FOLDER'] = extract_path
 
-    form1 = UploadFileForm()
-    if form1.validate_on_submit():
-        file = form.file.data # First grab the file
-        global input2
-        input2 = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        return "TMV and Counts have been uploaded."
-    return render_template('IDAX_to_TMV.html', form=form)
+#     form1 = UploadFileForm()
+#     if form1.validate_on_submit():
+#         file = form.file.data # First grab the file
+#         global input2
+#         input2 = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))
+#         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+#         return "TMV and Counts have been uploaded."
+#     return render_template('IDAX_to_TMV.html', form=form)
 
 
 @app.route('/process', methods=['POST'])
